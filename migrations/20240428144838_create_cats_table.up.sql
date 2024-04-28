55c19d7e31b53ec693dd3e6d280463dcedf8ca35
umar006 <umaralfaruq597@gmail.com>
2024-04-29 06:10:00 +0700

    feat: migration create table cats

---
+++ migrations/20240428144838_create_cats_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS cats (
+    id uuid PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULl,
+    name VARCHAR(30) NOT NULL,
+    race VARCHAR(24) NOT NULL,
+    sex VARCHAR(6) NOT NULL,
+    age_in_month SMALLINT NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    image_urls TEXT NOT NULL
+);
