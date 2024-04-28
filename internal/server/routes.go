39c9f31691d7678074deae0aa60043368265198d
umar006 <umaralfaruq597@gmail.com>
2024-04-29 06:14:44 +0700

    feat: create api to add new cat

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -23,6 +23,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	// cat
 	cat := apiV1.Group("/cat")
 	cat.GET("", func(c *gin.Context) { c.String(200, "HALO CAT") })
+	cat.POST("", handleAddNewCat)
 
 	// cat match
 	catMatch := cat.Group("/match")
