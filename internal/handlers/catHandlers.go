ec796e92752d0372d71a33845347248a577605e9
umar006 <umaralfaruq597@gmail.com>
2024-04-29 08:21:56 +0700

    feat: read request body

--- internal/handlers/catHandlers.go
+++ internal/handlers/catHandlers.go
@@ -1,9 +1,18 @@
 package handlers
 
 import (
+	"cats-social/internal/models"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func HandleAddNewCat(c *gin.Context) {
-	c.String(201, "ADD NEW CAT")
+	catBody := models.NewCat()
+	if err := c.ShouldBindJSON(&catBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(201, gin.H{"message": "success", "data": &catBody})
 }
