dad96293220d872808c7cebdab3985f87f60cba9
umar006 <umaralfaruq597@gmail.com>
2024-04-30 21:02:01 +0700

    fix: cat repository interface parameter

--- internal/repository/cat.go
+++ internal/repository/cat.go
@@ -6,21 +6,29 @@ import (
 )
 
 type CatRepository interface {
-	CreateCat(cat *domain.Cat) error
-	GetAllCats() ([]domain.Cat, error)
+	CreateCat(db *sql.DB, cat *domain.Cat) error
+	GetAllCats(db *sql.DB) ([]domain.Cat, error)
 }
 
 type CatRepositoryImpl struct {
 }
 
-func NewCatRepository(db *sql.DB) CatRepository {
+func NewCatRepository() CatRepository {
 	return &CatRepositoryImpl{}
 }
 
-func (c *CatRepositoryImpl) CreateCat(cat *domain.Cat) error {
+func (c *CatRepositoryImpl) CreateCat(db *sql.DB, catBody *domain.Cat) error {
+	// query := `INSERT INTO cats (id, created_at, name, race, sex, age_in_month, description, image_urls, owned_by)
+	// 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	// 	`
+	// _, err := db.Exec(query, catBody.ID, catBody.CreatedAt, catBody.Name, catBody.Race, catBody.Sex, catBody.AgeInMonth, catBody.Description, catBody.ImageUrls, catBody.OwnedBy)
+	// if err != nil {
+	// 	return err
+	// }
+
 	return nil
 }
 
-func (c *CatRepositoryImpl) GetAllCats() ([]domain.Cat, error) {
+func (c *CatRepositoryImpl) GetAllCats(db *sql.DB) ([]domain.Cat, error) {
 	return nil, nil
 }
